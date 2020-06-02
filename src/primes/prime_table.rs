49e6cad60a2dfacf9fbdc993f1dbb2f3b569cb2c
romo <robmoore121@gmail.com>
2020-06-02 19:05:26 +0800

    Exploit the csv primes to load them into memory slightly faster, which makes the tests run a bit faster

--- src/primes/prime_table.rs
+++ src/primes/prime_table.rs
@@ -11,13 +11,17 @@ pub struct PrimeTableReader<'a> {
 impl<'a> PrimeTableReader<'a> {
     pub fn new(directory_path: &Path) -> PrimeTableReader {
         assert!(directory_path.is_dir());
+        let first_million_primes_txt_path = directory_path.join("primes1.txt");
+        let first_million_primes_csv_path = directory_path.join("primes1.csv");
+        assert!(first_million_primes_txt_path.is_file() || first_million_primes_csv_path.is_file());
         return PrimeTableReader { directory_path };
     }
 
     pub fn first_million_from_file() -> Option<PrimeTableReader<'a>> {
         let prime_tables_path = Path::new("prime_tables");
-        let first_million_primes_file_path = prime_tables_path.join("primes1.txt");
-        return if prime_tables_path.is_dir() && first_million_primes_file_path.is_file() {
+        let first_million_primes_txt_path = prime_tables_path.join("primes1.txt");
+        let first_million_primes_csv_path = prime_tables_path.join("primes1.csv");
+        return if prime_tables_path.is_dir() && (first_million_primes_txt_path.is_file() || first_million_primes_csv_path.is_file()) {
             Some(PrimeTableReader::new(prime_tables_path))
         } else {
             None
@@ -25,18 +29,29 @@ impl<'a> PrimeTableReader<'a> {
     }
 
     pub fn first_million_primes(&self) -> Vec<ZPlus> {
-        let primes_text_file_path: PathBuf = self.directory_path.join("primes1.txt");
-        assert!(primes_text_file_path.is_file());
-        let mut primes_text_file: File = File::open(primes_text_file_path).unwrap();
-        let mut primes_text_content: String = String::new();
-        let bytes_read = primes_text_file.read_to_string(&mut primes_text_content).unwrap();
-        println!("Read {} bytes from prime table containing first million primes (primes1.txt)", bytes_read);
-        return PrimeTableReader::read_primes(primes_text_content);
+        let primes_csv_file_path: PathBuf = self.directory_path.join("primes1.csv");
+        let primes_txt_file_path: PathBuf = self.directory_path.join("primes1.txt");
+        return if primes_csv_file_path.is_file() {
+            let mut primes_text_file: File = File::open(primes_csv_file_path).unwrap();
+            let mut primes_text_content: String = String::new();
+            let bytes_read = primes_text_file.read_to_string(&mut primes_text_content).unwrap();
+            println!("Read {} bytes from prime table containing first million primes (primes1.csv)", bytes_read);
+            PrimeTableReader::read_primes_from_csv(primes_text_content)
+        } else if primes_txt_file_path.is_file() {
+            let mut primes_text_file: File = File::open(primes_txt_file_path).unwrap();
+            let mut primes_text_content: String = String::new();
+            let bytes_read = primes_text_file.read_to_string(&mut primes_text_content).unwrap();
+            println!("Read {} bytes from prime table containing first million primes (primes1.txt)", bytes_read);
+            PrimeTableReader::read_primes_from_table(primes_text_content)
+        } else {
+            panic!("Couldn't find either {:?} or {:?} inside directory {}",
+                   primes_csv_file_path, primes_txt_file_path, self.directory_path.display())
+        };
     }
 
-    fn read_primes(primes_text_content: String) -> Vec<ZPlus> {
+    fn read_primes_from_table(primes_txt_content: String) -> Vec<ZPlus> {
         let mut primes: Vec<ZPlus> = Vec::new();
-        for line in primes_text_content.lines() {
+        for line in primes_txt_content.lines() {
             // Skip over empty lines
             if line.trim().is_empty() || line.contains("Primes") {
                 continue;
@@ -51,7 +66,19 @@ impl<'a> PrimeTableReader<'a> {
                 }
             }
         }
+        return primes;
+    }
 
+    fn read_primes_from_csv(primes_csv_content: String) -> Vec<ZPlus> {
+        let mut primes: Vec<ZPlus> = Vec::new();
+        for entry in primes_csv_content.split(",") {
+            let i_optional = entry.trim().parse();
+            if i_optional.is_ok() {
+                primes.push(i_optional.unwrap());
+            } else {
+                panic!("Couldn't parse number from entry {}", entry)
+            }
+        }
         return primes;
     }
 }
