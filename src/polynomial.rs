6d00409d1f40730c2f785315cd04b675d8ae6b0c
romo <robmoore121@gmail.com>
2020-06-26 15:21:37 +0800

    Implement Add, Zero and One for polynomial struct, for use in Euclid's algorithm

--- src/polynomial.rs
+++ src/polynomial.rs
@@ -2,7 +2,7 @@ extern crate num;
 
 use std::cmp::PartialOrd;
 use std::fmt::Display;
-use std::ops::{Div, Mul, MulAssign, Sub};
+use std::ops::{Div, Mul, MulAssign, Sub, Add};
 
 use num::integer::lcm;
 use num::pow::pow;
@@ -349,10 +349,78 @@ impl Mul for Polynomial {
     }
 }
 
+impl Add for Polynomial {
+    type Output = Polynomial;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        // Ensure that self's degree is greater than or equal to rhs's degree
+        if rhs.degree() > self.degree() {
+            return rhs.add(self);
+        }
+
+        let mut new_coefficients = self.coefficients.clone();
+        let mut i = 0;
+        while i < rhs.coefficients.len() {
+            new_coefficients[i] = &self.coefficients[i] + &rhs.coefficients[i];
+            i += 1;
+        }
+        while i < self.coefficients.len() {
+            new_coefficients[i] = self.coefficients[i].clone();
+            i += 1;
+        }
+        Polynomial::truncate_coefficients(&mut new_coefficients);
+        Polynomial::new(new_coefficients)
+    }
+}
+
+impl Zero for Polynomial {
+    fn zero() -> Self {
+        Polynomial::new(vec![Z::zero()])
+    }
+
+    fn is_zero(&self) -> bool {
+        self.eq(&Polynomial::new(vec![Z::zero()]))
+    }
+}
+
+impl One for Polynomial {
+    fn one() -> Self {
+        Polynomial::new(vec![Z::one()])
+    }
+
+    fn is_one(&self) -> bool where
+        Self: PartialEq, {
+        self.eq(&Polynomial::new(vec![Z::one()]))
+    }
+}
+
 // TODO: Use quickcheck instead
 #[cfg(test)]
 mod polynomial_tests {
     use super::*;
+
+    #[test]
+    fn test_addition() {
+        // t^2 + 2t - 7 (+) t - 2
+        assert_eq!(Polynomial::from(vec![-2, 1])
+                       .add(Polynomial::from(vec![-7, 2, 1])),
+                   Polynomial::from(vec![-9, 3, 1]));
+
+        // t^2 - t - 10 (+) t + 2
+        assert_eq!(Polynomial::from(vec![-10, -1, 1])
+                       .add(Polynomial::from(vec![2, 1])),
+                   Polynomial::from(vec![-8, 0, 1]));
+
+        // 2t^3 - 7t^2 + 1 (+) t^2 - 1
+        assert_eq!(Polynomial::from(vec![1, 0, -7, 2])
+                       .add(Polynomial::from(vec![-1, 0, 1])),
+                   Polynomial::from(vec![0, 0, -6, 2]));
+
+        // t^2 + 2t - 7 (+) -t^2 + 2t - 7
+        assert_eq!(Polynomial::from(vec![-7, 2, 1])
+                       .add(Polynomial::from(vec![-7, 2, -1])),
+                   Polynomial::from(vec![-14, 4]));
+    }
 
     #[test]
     fn test_multiplication() {
