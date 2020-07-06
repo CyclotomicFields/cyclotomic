721c5179f2d078e3ff9346d5d41618f266600ec9
romo <robmoore121@gmail.com>
2020-07-06 18:59:59 +0800

    Start looking at += for polynomials

--- src/polynomial/add.rs
+++ src/polynomial/add.rs
@@ -1,4 +1,4 @@
-use std::ops::Add;
+use std::ops::{Add, AddAssign};
 
 use num::{Zero, zero};
 
@@ -38,6 +38,12 @@ impl Zero for Polynomial {
     }
 }
 
+impl AddAssign for Polynomial {
+    fn add_assign(&mut self, rhs: Self) {
+        unimplemented!()
+    }
+}
+
 // TODO: Use quickcheck instead
 #[cfg(test)]
 mod polynomial_tests {
@@ -64,5 +70,29 @@ mod polynomial_tests {
         assert_eq!(Polynomial::from(vec![-7, 2, 1])
                        .add(Polynomial::from(vec![-7, 2, -1])),
                    Polynomial::from(vec![-14, 4]));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_mut_addition() {
+        // t^2 + 2t - 7 (+) t - 2
+        let mut p1 = Polynomial::from(vec![-2, 1]);
+        p1.add_assign(Polynomial::from(vec![-7, 2, 1]));
+        assert_eq!(p1, Polynomial::from(vec![-9, 3, 1]));
+
+        // t^2 - t - 10 (+) t + 2
+        let mut p2 = Polynomial::from(vec![-10, -1, 1]);
+        p2.add_assign(Polynomial::from(vec![2, 1]));
+        assert_eq!(p2, Polynomial::from(vec![-8, 0, 1]));
+
+        // 2t^3 - 7t^2 + 1 (+) t^2 - 1
+        let mut p3 = Polynomial::from(vec![1, 0, -7, 2]);
+        p3.add_assign(Polynomial::from(vec![-1, 0, 1]));
+        assert_eq!(p3, Polynomial::from(vec![0, 0, -6, 2]));
+
+        // t^2 + 2t - 7 (+) -t^2 + 2t - 7
+        let mut p4 = Polynomial::from(vec![-7, 2, 1]);
+        p4.add_assign(Polynomial::from(vec![-7, 2, -1]));
+        assert_eq!(p4, Polynomial::from(vec![-14, 4]));
     }
 }
