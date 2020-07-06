5a575ebc05a2b2063ea26b2e5e70fa2c1dbb8cb1
romo <robmoore121@gmail.com>
2020-07-06 19:13:50 +0800

    Implement add_mut for polynomials

--- src/polynomial/add.rs
+++ src/polynomial/add.rs
@@ -3,28 +3,49 @@ use std::ops::{Add, AddAssign};
 use num::{Zero, zero};
 
 use crate::polynomial::polynomial::{Polynomial, Z, Q};
+use std::cmp::max;
+
+impl Polynomial {
+    fn add_mut(&mut self, rhs: Self) {
+        if self.degree() >= rhs.degree() {
+            let mut i = 0;
+            while i < rhs.coefficients.len() {
+                self.coefficients[i] += &rhs.coefficients[i];
+                i += 1;
+            }
+        } else {
+            let mut i = 0;
+            while i < self.coefficients.len() {
+                self.coefficients[i] += &rhs.coefficients[i];
+                i += 1;
+            }
+            // This is suboptimal. It could be implemented faster by doing a
+            // bulk copy of the slice of data corresponding to the additional
+            // coefficients present in the RHS polynomial.
+            self.coefficients.extend(vec![Q::zero();rhs.degree() - self.degree()]);
+            while i < rhs.coefficients.len() {
+                self.coefficients[i] = rhs.coefficients[i].clone();
+                i += 1;
+            }
+        }
+        Polynomial::truncate_coefficients(&mut self.coefficients)
+    }
+}
 
 impl Add for Polynomial {
     type Output = Polynomial;
 
     fn add(self, rhs: Self) -> Self::Output {
-        // Ensure that self's degree is greater than or equal to rhs's degree
-        if rhs.degree() > self.degree() {
-            return rhs.add(self);
-        }
+        // Possibly suboptimal
+        let mut clone = self.clone();
+        clone.add_mut(rhs);
+        clone
+    }
+}
 
-        let mut new_coefficients = self.coefficients.clone();
-        let mut i = 0;
-        while i < rhs.coefficients.len() {
-            new_coefficients[i] = &self.coefficients[i] + &rhs.coefficients[i];
-            i += 1;
-        }
-        while i < self.coefficients.len() {
-            new_coefficients[i] = self.coefficients[i].clone();
-            i += 1;
-        }
-        Polynomial::truncate_coefficients(&mut new_coefficients);
-        Polynomial::new(new_coefficients)
+impl AddAssign for Polynomial {
+    fn add_assign(&mut self, rhs: Self) {
+        self.add_mut(rhs);
     }
 }
 
@@ -35,12 +56,6 @@ impl Zero for Polynomial {
 
     fn is_zero(&self) -> bool {
         self.eq(&zero())
-    }
-}
-
-impl AddAssign for Polynomial {
-    fn add_assign(&mut self, rhs: Self) {
-        unimplemented!()
     }
 }
 
@@ -73,7 +88,6 @@ mod polynomial_tests {
     }
 
     #[test]
-    #[ignore]
     fn test_mut_addition() {
         // t^2 + 2t - 7 (+) t - 2
         let mut p1 = Polynomial::from(vec![-2, 1]);
@@ -94,5 +108,10 @@ mod polynomial_tests {
         let mut p4 = Polynomial::from(vec![-7, 2, 1]);
         p4.add_assign(Polynomial::from(vec![-7, 2, -1]));
         assert_eq!(p4, Polynomial::from(vec![-14, 4]));
+
+        // t^2 - 1 (+) 2t^3 - 7t^2 + 1
+        let mut p5 = Polynomial::from(vec![-1, 0, 1]);
+        p5.add_assign(Polynomial::from(vec![1, 0, -7, 2]));
+        assert_eq!(p5, Polynomial::from(vec![0, 0, -6, 2]));
     }
 }
