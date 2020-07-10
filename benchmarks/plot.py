adda7c6413c225994858154390880d5f86b4db50
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2020-07-11 00:20:08 +0100

    Clean up plot.py a bit more

--- benchmarks/plot.py
+++ benchmarks/plot.py
@@ -21,22 +21,9 @@ args = parser.parse_args()
 NEXT_FIGURE = 1
 log = args.log
 
-def make_pretty_name(fname):
-    """Prettifies a file name for use in a legend"""
-    s = fname.split('.')[0] # remove .csv
-    old_words = s.split('_')[1:] # remove first word
-    words = []
-    for word in old_words:
-        if word == 'mymethod':
-            words.append('our method')
-        else:
-            words.append(word)
-    ret = ' '.join([word.title() for word in words])
-    return ret
-
 def do_plot_multiple(results_mult, xindex, yindex, xlabel, ylabel, title, fname, labels):
     global NEXT_FIGURE, log
-    marker_color = itertools.cycle(itertools.product("krgbcmy", "x+."))
+    marker_color = itertools.cycle(itertools.product("rgbcmy", "x+."))
     plt.figure(NEXT_FIGURE)
     NEXT_FIGURE += 1
     plt.title(title)
@@ -67,25 +54,13 @@ def read_pts(fname):
 
     return np.array([[float(s) for s in line.split(" ")] for line in data])
 
-# plots a file with cols:
-# group size | group id | nr conjugacy classes | degree | time taken
-def plot(fname):
-    pts = read_pts(fname)
-    to_plot = pts.T
-    do_plot(to_plot, 0, 4, "group size", "time taken", f'{fname}: |G| vs t', f'{fname}_size')
-    do_plot(to_plot, 2, 4, "num classes", "time taken", f'{fname}: |cc(G)| vs t', f'{fname}_classes')
-    do_plot(to_plot, 3, 4, "degree", "time taken", f'{fname}: deg vs t', f'{fname}_degree')
-
 # plots multiple files on the same graph, for comparison
 def plot_multiple(fnames, out_fname):
     time_taken = "time taken (ns)"
     if log:
         time_taken = "log(time taken (ns))"
     ptss = [read_pts(fname) for fname in fnames]
-    labels = [make_pretty_name(fname) for fname in fnames]
-    do_plot_multiple(ptss, 0, 4, "$|G|$", time_taken, f'', f'{out_fname}_size', labels)
-    do_plot_multiple(ptss, 2, 4, "num classes", time_taken, f'', f'{out_fname}_classes', labels)
-    do_plot_multiple(ptss, 3, 4, "degree", time_taken, f'', f'{out_fname}_degree', labels)
+    do_plot_multiple(ptss, 0, 1, "$n$", time_taken, f'', f'{out_fname}_order', fnames)
 
 def tolatex(data, header, output_file):
     output = ""
@@ -100,7 +75,7 @@ def tolatex(data, header, output_file):
     output += '\n\\end{tabular}\n'
     open(output_file, "w").write(output)
 
-header = ["|G|", "id", "|cc(G)|", "degree", "time taken"]
+header = ["n", "t"]
 
 fnames = args.file
 plot_multiple(fnames, args.prefix)
