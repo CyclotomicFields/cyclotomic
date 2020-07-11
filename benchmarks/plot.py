0b031cdf91390647b7c5692536894235cb7e1a95
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2020-07-11 22:59:19 +0100

    Generalise plot.py script more

--- benchmarks/plot.py
+++ benchmarks/plot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib, glob, sys, itertools, math
+import csv
 
 # Required pre plt import configuration
 matplotlib.rcParams['pgf.rcfonts'] = False
@@ -12,10 +13,12 @@ matplotlib.rcParams['figure.figsize'] = "4,4"
 import matplotlib.pyplot as plt
 
 from argparse import ArgumentParser
-parser = ArgumentParser(description="Generate graphs from benchmark data")
-parser.add_argument("--log", help="use a logarithmic scale for the y axis", action="store_true")
-parser.add_argument("--prefix", help="prefix to use for the files generated", default="default_prefix")
-parser.add_argument("file", help="files to graph", nargs="+")
+parser = ArgumentParser(description="Generate graphs from csv benchmark data")
+parser.add_argument("-l", "--log", help="use a logarithmic scale for the y axis", action="store_true")
+parser.add_argument("-p", "--prefix", help="prefix to use for the files generated", default="default_prefix")
+parser.add_argument("-x", "--x", help="column to use as x axis and label", nargs=2, metavar=("column_index", "label"), required=True)
+parser.add_argument("-y", "--y", help="column to use as y axis and label", nargs=2, metavar=("column_index", "label"), required=True)
+parser.add_argument("file", metavar="file.csv", help="csv files to graph", nargs="+")
 args = parser.parse_args()
 
 NEXT_FIGURE = 1
@@ -47,20 +50,17 @@ def do_plot_multiple(results_mult, xindex, yindex, xlabel, ylabel, title, fname,
 def read_pts(fname):
     data = []
 
-    try:
-        data = open(fname, "r").read().splitlines()
-    except FileNotFoundError:
-        return
-
-    return np.array([[float(s) for s in line.split(" ")] for line in data])
+    with open(fname, "r", newline="") as in_csv:
+        reader = csv.reader(in_csv, delimiter=",")
+        return np.array([[float(s) for s in row] for row in reader])
 
 # plots multiple files on the same graph, for comparison
-def plot_multiple(fnames, out_fname):
-    time_taken = "time taken (ns)"
+def plot_multiple(fnames, out_fname, header):
+    y_label = header[1]
     if log:
-        time_taken = "log(time taken (ns))"
+        y_label = f"log({y_label})"
     ptss = [read_pts(fname) for fname in fnames]
-    do_plot_multiple(ptss, 0, 1, "$n$", time_taken, f'', f'{out_fname}_order', fnames)
+    do_plot_multiple(ptss, 0, 1, header[0], y_label, f'', f'{out_fname}', fnames)
 
 def tolatex(data, header, output_file):
     output = ""
@@ -75,13 +75,13 @@ def tolatex(data, header, output_file):
     output += '\n\\end{tabular}\n'
     open(output_file, "w").write(output)
 
-header = ["n", "t"]
+header = [args.x[1], args.y[1]]
 
 fnames = args.file
-plot_multiple(fnames, args.prefix)
+plot_multiple(fnames, args.prefix, header)
 
-for result_csv in fnames:
-    with open(result_csv, "r") as result_file:
-        lines = result_file.readlines()
-        results = [[int(x) for x in row.split(" ")] for row in lines]
-        tolatex(results, header, result_csv+".tex")
+for input_csv in fnames:
+    with open(input_csv, "r", newline="") as input_file:
+        csv_reader = csv.reader(input_file, delimiter=",")
+        results = [[int(x) for x in row] for row in csv_reader]
+        tolatex(results, header, input_csv + ".tex")
