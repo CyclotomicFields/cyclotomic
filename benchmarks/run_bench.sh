612a49d1ab061c604128ca61596d5f48d6cbcbee
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2020-07-14 23:36:06 +0100

    Fix run_bench.sh for loop

--- benchmarks/run_bench.sh
+++ benchmarks/run_bench.sh
@@ -7,9 +7,9 @@ step=$4
 d="results"
 mkdir -p "$d"
 
-for n in {$min_order..$max_order..$step}; do
+for ((n=min_order; n <= max_order; n += step)); do
     echo "n=$n"
-    echo "$n,$(antic/anticbench $n $num_tests)" >> $d/antic
+    echo "$n,$(../target/release/cyclobench -i antic -l $n -u $((n+1)) -n $num_tests)" >> $d/antic
     echo "$n,$(../target/release/cyclobench -l $n -u $((n+1)) -n $num_tests -g test.g)" >> $d/cyclotomic
     echo "$n,$(gap -q < test.g)" >> $d/gap
 done
