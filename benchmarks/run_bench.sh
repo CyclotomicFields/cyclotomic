06c2f8cfa7fd9123844fd8a0a506fce68508727e
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2020-07-14 23:00:38 +0100

    Add more params to run_bench.sh script

--- benchmarks/run_bench.sh
+++ benchmarks/run_bench.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -x
 
 num_tests=$1
+min_order=$2
+max_order=$3
+step=$4
 d="results"
 mkdir -p "$d"
 
-for n in {10..200..10}; do
+for n in {$min_order..$max_order..$step}; do
     echo "n=$n"
     echo "$n,$(antic/anticbench $n $num_tests)" >> $d/antic
     echo "$n,$(../target/release/cyclobench -l $n -u $((n+1)) -n $num_tests -g test.g)" >> $d/cyclotomic
