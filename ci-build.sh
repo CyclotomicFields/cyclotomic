8789bc6efc81e25b20ed41d832cbe107310a2172
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2020-08-10 21:33:03 +0100

    Add sudo

--- ci-build.sh
+++ ci-build.sh
@@ -2,7 +2,7 @@
 
 antic_version=0.2.2
 
-apt install -y libgmp-dev libmpfr-dev libflint-2.6.2 libflint-dev
+sudo apt install -y libgmp-dev libmpfr-dev libflint-2.6.2 libflint-dev
 mkdir build
 cd build
 wget https://github.com/wbhart/antic/archive/${antic_version}.tar.gz
@@ -10,4 +10,4 @@ tar -xvf ${antic_version}.tar.gz
 cd antic-${antic_version}
 ./configure
 make
-make install
+sudo make install
