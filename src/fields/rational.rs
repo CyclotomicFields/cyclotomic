23fa5e1bcb2e7b6c00dcc07d5b5e60a1f27354ee
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2020-09-19 22:25:51 +0100

    Add f64 fake rational impl

--- src/fields/rational.rs
+++ src/fields/rational.rs
@@ -1,36 +1,41 @@
-use crate::fields::{AdditiveGroupElement, FieldElement, MultiplicativeGroupElement};
+use crate::fields::{AdditiveGroupElement, FieldElement, MultiplicativeGroupElement, Z};
 use std::fmt;
-use std::fmt::Display;
+use std::fmt::{Debug, Display};
+use std::hash::{Hash, Hasher};
+use std::mem::transmute;
 use std::ops::{AddAssign, MulAssign};
 
-pub trait Rational: FieldElement + Clone + From<(i64, u64)> + Display {
+pub trait Rational:
+    FieldElement + Clone + From<(i64, u64)> + From<i64> + Display + Send + Debug + PartialEq + Eq + Hash
+{
     fn zero() -> Self {
         Self::from((0, 1))
     }
+
     fn is_zero(&self) -> bool;
+
+    fn numer(&self) -> Z;
+
+    fn denom(&self) -> Z;
 }
 
-impl<T: Rational> From<i64> for T {
-    fn from(n: i64) -> Self {
-        Self::from((n, 1))
-    }
-}
+// TODO: there are a lot of clones due to the &mut interface, need to change
 
 impl AdditiveGroupElement for rug::Rational {
     fn add(&mut self, z: &mut Self) -> &mut Self {
-        self.add_assign(z);
+        *self += z.clone();
         self
     }
 
     fn add_invert(&mut self) -> &mut Self {
-        self.neg_assign();
+        *self = self.as_neg().clone();
         self
     }
 }
 
 impl MultiplicativeGroupElement for rug::Rational {
     fn mul(&mut self, z: &mut Self) -> &mut Self {
-        self.mul_assign(z);
+        *self *= z.clone();
         self
     }
 
@@ -42,7 +47,7 @@ impl MultiplicativeGroupElement for rug::Rational {
 
 impl FieldElement for rug::Rational {
     fn eq(&mut self, other: &mut Self) -> bool {
-        self == other
+        *self == *other
     }
 }
 
@@ -51,8 +56,16 @@ impl Rational for rug::Rational {
         rug::Rational::from(0)
     }
 
-    fn is_zero(&self) -> Self {
+    fn is_zero(&self) -> bool {
         self.is_zero()
+    }
+
+    fn numer(&self) -> Z {
+        self.numer().clone()
+    }
+
+    fn denom(&self) -> Z {
+        self.denom().clone()
     }
 }
 
@@ -79,6 +92,12 @@ impl FixedSizeRational {
     }
 }
 
+impl From<i64> for FixedSizeRational {
+    fn from(n: i64) -> Self {
+        Self::from((n, 1))
+    }
+}
+
 impl Display for FixedSizeRational {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}/{}", self.num, self.denom)
@@ -87,12 +106,12 @@ impl Display for FixedSizeRational {
 
 impl From<(i64, u64)> for FixedSizeRational {
     fn from(p: (i64, u64)) -> Self {
-        Rat::new(p.0, p.1)
+        FixedSizeRational::new(p.0, p.1)
     }
 }
 
 impl AdditiveGroupElement for FixedSizeRational {
-    fn add(&mut self, other: &Self) -> &mut Self {
+    fn add(&mut self, other: &mut Self) -> &mut Self {
         let common_denom = num::integer::lcm(self.denom, other.denom);
         self.num = self.num * (common_denom / self.denom) as i64
             + other.num * (common_denom / other.denom) as i64;
@@ -108,7 +127,7 @@ impl AdditiveGroupElement for FixedSizeRational {
 }
 
 impl MultiplicativeGroupElement for FixedSizeRational {
-    fn mul(&mut self, other: &Self) -> &mut Self {
+    fn mul(&mut self, other: &mut Self) -> &mut Self {
         self.num *= other.num;
         self.denom *= other.denom;
         self.reduce();
@@ -126,7 +145,7 @@ impl MultiplicativeGroupElement for FixedSizeRational {
 
 impl FieldElement for FixedSizeRational {
     fn eq(&mut self, other: &mut Self) -> bool {
-        self == other
+        *self == *other
     }
 }
 
@@ -135,7 +154,99 @@ impl Rational for FixedSizeRational {
         FixedSizeRational::from((0, 1))
     }
 
-    fn is_zero(&self) -> Self {
+    fn is_zero(&self) -> bool {
         self.is_zero()
+    }
+
+    fn numer(&self) -> Z {
+        Z::from(self.num)
+    }
+
+    fn denom(&self) -> Z {
+        Z::from(self.denom)
+    }
+}
+
+// floats aren't rationals, but they are very fast...
+#[derive(Debug, Clone, PartialEq)]
+struct FloatRational(f64);
+
+impl Eq for FloatRational {}
+
+// maybe a hack
+impl Hash for FloatRational {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        let x: u64 = unsafe { transmute(self.0) };
+        x.hash(state);
+    }
+}
+
+impl Display for FloatRational {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl From<(i64, u64)> for FloatRational {
+    fn from(p: (i64, u64)) -> Self {
+        FloatRational(p.0 as f64 / p.1 as f64)
+    }
+}
+
+impl From<i64> for FloatRational {
+    fn from(n: i64) -> Self {
+        FloatRational(n as f64)
+    }
+}
+
+impl AdditiveGroupElement for FloatRational {
+    fn add(&mut self, other: &mut Self) -> &mut Self {
+        self.0 += other.0;
+        self
+    }
+
+    fn add_invert(&mut self) -> &mut Self {
+        self.0 *= -1_f64;
+        self
+    }
+}
+
+impl MultiplicativeGroupElement for FloatRational {
+    fn mul(&mut self, other: &mut Self) -> &mut Self {
+        self.0 *= other.0;
+        self
+    }
+
+    fn mul_invert(&mut self) -> &mut Self {
+        self.0 = 1_f64 / self.0;
+        self
+    }
+}
+
+impl FieldElement for FloatRational {
+    fn eq(&mut self, other: &mut Self) -> bool {
+        *self == *other
+    }
+}
+
+impl Rational for FloatRational {
+    fn zero() -> Self {
+        FloatRational(0_f64)
+    }
+
+    fn is_zero(&self) -> bool {
+        self.0 == 0_f64
+    }
+
+    // TODO: fix the API so I don't have to lie here
+    fn numer(&self) -> Z {
+        // this is a hack, possible only because I know this is usually used to
+        // get an integer from the rational.
+        Z::from(self.0 as i64)
+    }
+
+    fn denom(&self) -> Z {
+        // this is almost always wrong
+        Z::from(1)
     }
 }
